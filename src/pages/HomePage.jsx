ee4f16c8d4a822e9ff4530baba23318446322246
Lorenzo Rodigliano <rodiglianolorenzo@gmail.com>
2026-04-21 12:40:45 +0200

    complete milestone 3

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,27 @@
+import { useState, useEffect } from 'react'
 
 export default function HomePage() {
 
-    return (<div>Hello, World!</div>
+    const [films, setFilms] = useState([])
 
+    useEffect(() => {
+        const api_url = import.meta.env.VITE_API_SERVER_ADDRESS + '/films'
+
+        fetch(api_url)
+            .then(response => response.json())
+            .then(data => setFilms(data))
+    }, [])
+
+
+    return (
+        <div>
+            {films.map(film => (
+                <div key={film.id}>
+                    <h2>{film.title}</h2>
+                    <p>{film.genre}</p>
+                    <p>{film.release_year}</p>
+                </div>
+            ))}
+        </div>
     )
-
 }
